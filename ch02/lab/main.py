da7a2cca1efdc84106f62c889f436571369efa20
Blake Abrenica <112440148+BlakeAbr@users.noreply.github.com>
2022-09-09 15:49:28 +0000

    Created variable classes_per_week and cost_per_class. Also created a list and put it through random.choice

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,21 @@
-
+import random
 #Part A
+classes_per_week = 3
+
 weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
+
+cost_per_class = cost_per_week / classes_per_week
+
 print("Cost per week:", cost_per_week)
+print("Cost per class:", cost_per_class )
+## Only two variables I created was classes_per_week and cost_per_class
+print(classes_per_week, type(classes_per_week), cost_per_class, type(cost_per_class))
 
 
 #Part B
+my_list = ["Blake", 2, 6.9, "doug dimmadome owner of the dimmsdale dimmadome", 10]
+print(random.choice(my_list))
+
