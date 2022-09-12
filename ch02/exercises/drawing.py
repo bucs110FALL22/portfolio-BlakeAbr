641c75d3a664df41a53c0be979990368e728439e
Blake Abrenica <112440148+BlakeAbr@users.noreply.github.com>
2022-09-12 21:54:57 +0000

    Completed the Drawing with Turtle Exercise

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -1,14 +1,17 @@
 import turtle
-sides = int(input("How many sides do you want the shape to have?"))
-length = int(input("How long do you want each side to be?"))
+
+sides = int(input("How many sides do you want the shape to have? "))
+length = int(input("How long do you want each side to be? "))
+color_of_turtle = input("What color would you like the turtle to be? ")
 my_turtle = turtle.Turtle()
 my_turtle.shape("turtle")
-my_turtle.color("blue")
+my_turtle.color(color_of_turtle)
 angle = 360/sides
 
-for i in [angle]*sides:
+for i in [angle] * sides:
   my_turtle.forward(length)
   my_turtle.left(i)
 
-
 turtle.exitonclick()
+
+
