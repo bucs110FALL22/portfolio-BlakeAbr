46ba92c79095a39f0b98a512bd9ad2e9a9f28baa
Blake Abrenica <112440148+BlakeAbr@users.noreply.github.com>
2022-10-10 21:01:34 +0000

    Created the Ch05 Printing Patterns Exercise

---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,19 @@
+#Function that creates normal pyramid
+def star_pyramid():
+  rows = int(input("Please enter the number of rows for a pyramid: "))
+  for i in range(rows):
+    for j in range(i+1):
+        print("*", end="")
+    print("\n")
+    
+star_pyramid()
+#Function that creates a reverse pyramid
+def rstar_pyramid():
+  rows = int(input("Please enter the number of rows for a reverse pyramid: "))
+  for i in range(rows, 0, -1):
+    for j in range(0, i):
+        print("*", end="")
+    
+    print("\n")
+
+rstar_pyramid()
